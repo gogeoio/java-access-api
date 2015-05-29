3f5c47ace4940b5f097f325037aad0c65fc9f4c4
Everton Lima <everton.lima@cuia.com.br>
2015-05-29 15:34:02 -0300

    Adding README.md.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # java-access-api
+
+This is an example of how to use **goGeo** API in Java.
+
+The source code is based *retrofit* rest client, this was choiced by the ease of conversion of Java Objects to JSON and vice-versa. How ever,  **goGeo** API can be used with any rest client.
+
+In this example just some services was added. You are well come to fork this repository and improve it.
+
+## Endpoints implemented
+ 
+ * POST /database
+ * GET  /database
+ * GET  /databases/{database}
+ * POST /databases/{database}/collections
+ * GET  /databases/{database}/collections
+ * GET  /databases/{database}/collections/{collection}
+ * POST /databases/{database}/collections/{collection}/documents.json
+ * GET  /databases/{database}/collections/{collection}/documents
+
+
+## Usage
+
+1. Clone this repository
+2. Import it as *Maven project* in Eclipse IDE
+3. Configure the following variables in *io.gogeo.app.App.java* file
+3.1. APIKEY
+3.2. DATABASENAME
+3.2. COLLECTIONNAME
+4. Run as Java project
+
+## Explanations
+
+All logic of this example is in *io.gogeo.app.App.java* file.
+You can set your APIKEY, DATABASENAME and COLLECTIONNAME in the begin file. To the document to insert, change the properties of document in *generateDocumentToInsert()* method. 
+
+Notice that, if the collection already has documents, the insertion will fail if the new one does not follow the same attribute structures. If it is the first document the collection, the structure of layer will be a reflection of this document.
--- src/main/java/io/gogeo/app/App.java
+++ src/main/java/io/gogeo/app/App.java
@@ -21,145 +21,153 @@ import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 
 public class App {
-  private final static String APIKEY = "123";
-  private final static String ENDPOINT = "http://api.gogeo.io/1.0";
-  private final static String DATABASENAME = "db_test";
-  private final static String COLLECTIONNAME = "coll_test2";
-  
-  private final RequestInterceptor requestInterceptor;
-  private final RestAdapter restAdapter;
-  private final GogeoService service;
-  
-  /**
-   * This works just in Java 7 or high. If you are using a lower version comment this method
-   * 
-   * @return
-   */
-  @SuppressWarnings("unused")
-  private String getJava7NativeAuthorization() {
-    final String userAndPassword = APIKEY + ":";
-    String encoded = DatatypeConverter.printBase64Binary(userAndPassword
-        .getBytes());
-    return "Basic " + encoded;
-  }
+	private final static String APIKEY = "123";
+	private final static String ENDPOINT = "http://api.gogeo.io/1.0";
+	private final static String DATABASENAME = "db_test";
+	private final static String COLLECTIONNAME = "coll_test2";
 
-  /**
-   * This method is the same proposal of 'getJava7NativeAuthorization', but
-   * your performance is 4 times slower. This works with apache commoms codec.
-   * 
-   * @return
-   */
-  private String getApacheAuthorization() {
-    final String userAndPassword = APIKEY + ":";
-    String encoded = Base64.encodeBase64String(userAndPassword.getBytes());
-    return "Basic " + encoded;
-  }
+	private final RequestInterceptor requestInterceptor;
+	private final RestAdapter restAdapter;
+	private final GogeoService service;
 
-  public App() {
-	  requestInterceptor = new RequestInterceptor() {
-	      public void intercept(RequestFacade request) {
-	        request.addHeader("Authorization", getApacheAuthorization());
-	      }
-	    };
+	/**
+	 * This works just in Java 7 or high. If you are using a lower version
+	 * comment this method
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unused")
+	private String getJava7NativeAuthorization() {
+		final String userAndPassword = APIKEY + ":";
+		String encoded = DatatypeConverter.printBase64Binary(userAndPassword
+				.getBytes());
+		return "Basic " + encoded;
+	}
 
-	    // Creating a Rest adapter to do request and add the interceptor to it 
-	    restAdapter = new RestAdapter.Builder()
-	        .setEndpoint(ENDPOINT)
-	        .setRequestInterceptor(requestInterceptor).build();
+	/**
+	 * This method is the same proposal of 'getJava7NativeAuthorization', but
+	 * your performance is 4 times slower. This works with apache commoms codec.
+	 * 
+	 * @return
+	 */
+	private String getApacheAuthorization() {
+		final String userAndPassword = APIKEY + ":";
+		String encoded = Base64.encodeBase64String(userAndPassword.getBytes());
+		return "Basic " + encoded;
+	}
 
-	    // Adding gogeo services defined in GogeoService class to the rest Client (retrofit)
-	    service = restAdapter.create(GogeoService.class);
-	    
-	    execute();
-  }
-  
-  public void list() {
-	  List<Document> docs = service.listDocuments(DATABASENAME, COLLECTIONNAME);
-	  System.out.println("[");
-	  for(Document d: docs) {
-		  System.out.println("{");
-		  for(Entry<String, Object> en: d.entrySet()) {
-			  System.out.println(en.getKey() + " : "+en.getValue());
-		  }
-		  System.out.println("}");
-	  }
-	  System.out.println("]");
-  }
+	public App() {
+		requestInterceptor = new RequestInterceptor() {
+			public void intercept(RequestFacade request) {
+				request.addHeader("Authorization", getApacheAuthorization());
+			}
+		};
 
-  public static void main(String[] args) {
-    new App();
-  }
+		// Creating a Rest adapter to do request and add the interceptor to it
+		restAdapter = new RestAdapter.Builder().setEndpoint(ENDPOINT)
+				.setRequestInterceptor(requestInterceptor).build();
 
-  /**
-   * The Execution of a simple sample of gogeo usage in JAVA.
-   */
-  public void execute() {
-    // Define the interceptor, add authentication headers. All request will pass by 'intercept' method.
-    
+		// Adding gogeo services defined in GogeoService class to the rest
+		// Client (retrofit)
+		service = restAdapter.create(GogeoService.class);
 
-    // Try to create the database, if it exist an error is printed on screem with Error Code 400
-    try {
-      Database database = new Database();
-      database.setDatabaseName(DATABASENAME);
-      service.createDatabase(database);
-    } catch (Exception e) {
-      System.err.println("Error in create database: " + e.getMessage());
-    }
+		execute();
+	}
 
-    // List the Databases of the user.
-    List<Database> databases = service.listDatabases();
-    for (Database db : databases) {
-      System.out.println("\t"+db.getId() + " --> " + db.getDatabaseName());
-    }
+	public void list() {
+		List<Document> docs = service.listDocuments(DATABASENAME,
+				COLLECTIONNAME);
+		System.out.println("[");
+		for (Document d : docs) {
+			System.out.println("{");
+			for (Entry<String, Object> en : d.entrySet()) {
+				System.out.println(en.getKey() + " : " + en.getValue());
+			}
+			System.out.println("}");
+		}
+		System.out.println("]");
+	}
+	
+	public DocumentInsert generateDocumentToInsert() {
+		Geom geom = new Geom();
+		ArrayList<Float> coordinates = new ArrayList<Float>();
+		coordinates.add(new Float(-118.14));
+		coordinates.add(new Float(-34.18));
+		geom.setCoordinates(coordinates);
+		geom.setType("Point");
 
-    // Try to create the collection, if it exist an error is printed on screem with Error Code 400
-    try {
-      Collection collection = new Collection();
-      collection.setCollectionName(COLLECTIONNAME);
-      service.createCollection(DATABASENAME, collection);
-    } catch (Exception e) {
-      System.err.println("Error in create collection: " + e.getMessage());
-    }
+		Properties props = new Properties();
+		props.put("code_text", "123456");
+		props.put("name_text", "Roberto Rodrigues Junior");
+		props.put("employees_int", 500);
+		props.put("revenue_long", 25);
 
-    // List the Collections in the Database.
-    List<Collection> collections = service.listCollections(DATABASENAME);
-    System.out.println("Collections in database: " + DATABASENAME);
-    for (Collection coll : collections) {
-      System.out.println("\t" + coll.getId() + " --> "
-          + coll.getCollectionName() + ": " + coll.getItems());
-    }
-    System.out.println();
+		JSON json = new JSON();
+		json.setGeom(geom);
+		json.setProperties(props);
 
-    // Insert one document.
-    Geom geom = new Geom();
-    ArrayList<Float> coordinates = new ArrayList<Float>();
-    coordinates.add(new Float(-118.14));
-    coordinates.add(new Float(-34.18));
-    geom.setCoordinates(coordinates);
-    geom.setType("Point");
+		List<JSON> jsonDocs = new ArrayList<JSON>(1);
+		jsonDocs.add(json);
 
-    Properties props = new Properties();
-    props.put("code_text", "123456");
-    props.put("name_text", "Roberto Rodrigues Junior");
-    props.put("employees_int", 500);
-    props.put("revenue_long", 25);
+		DocumentInsert document = new DocumentInsert();
+		document.setJson(jsonDocs);
+		
+		return document;
+	}
 
-    JSON json = new JSON();
-    json.setGeom(geom);
-    json.setProperties(props);
+	public static void main(String[] args) {
+		new App();
+	}
 
-    List<JSON> jsonDocs = new ArrayList<JSON>(1);
-    jsonDocs.add(json);
+	/**
+	 * The Execution of a simple sample of gogeo usage in JAVA.
+	 */
+	public void execute() {
+		// Define the interceptor, add authentication headers. All request will
+		// pass by 'intercept' method.
 
-    DocumentInsert document = new DocumentInsert();
-    document.setJson(jsonDocs);
+		// Try to create the database, if it exist an error is printed on screem
+		// with Error Code 400
+		try {
+			Database database = new Database();
+			database.setDatabaseName(DATABASENAME);
+			service.createDatabase(database);
+		} catch (Exception e) {
+			System.err.println("Error in create database: " + e.getMessage());
+		}
 
-    try {
-      service.insertDocument(DATABASENAME, COLLECTIONNAME, document);
-      list();
-    } catch (Exception e) {
-      System.err.println("Error in insert document: " + e.getMessage());
-      e.printStackTrace();
-    }
-  }
+		// List the Databases of the user.
+		List<Database> databases = service.listDatabases();
+		for (Database db : databases) {
+			System.out.println("\t" + db.getId() + " --> "
+					+ db.getDatabaseName());
+		}
+
+		// Try to create the collection, if it exist an error is printed on
+		// screem with Error Code 400
+		try {
+			Collection collection = new Collection();
+			collection.setCollectionName(COLLECTIONNAME);
+			service.createCollection(DATABASENAME, collection);
+		} catch (Exception e) {
+			System.err.println("Error in create collection: " + e.getMessage());
+		}
+
+		// List the Collections in the Database.
+		List<Collection> collections = service.listCollections(DATABASENAME);
+		System.out.println("Collections in database: " + DATABASENAME);
+		for (Collection coll : collections) {
+			System.out.println("\t" + coll.getId() + " --> "
+					+ coll.getCollectionName() + ": " + coll.getItems());
+		}
+		System.out.println();
+
+		try {
+			service.insertDocument(DATABASENAME, COLLECTIONNAME, generateDocumentToInsert());
+			list();
+		} catch (Exception e) {
+			System.err.println("Error in insert document: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }
